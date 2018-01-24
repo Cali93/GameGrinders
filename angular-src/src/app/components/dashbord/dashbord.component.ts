492eaa76e0c3689140d6847b73b61505a3edd7dc
Cali93 <c.armut93@outlook.com>
2018-01-24 19:25:45 +0100

    auth working

---
+++ angular-src/src/app/components/dashbord/dashbord.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+// import { ScenegraphComponent } from './components/scenegraph/scenegraph.component';
+
+@Component({
+  selector: 'app-dashbord',
+  templateUrl: './dashbord.component.html',
+  styleUrls: ['./dashbord.component.css']
+})
+export class DashbordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
